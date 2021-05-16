4833474d94af7d7e60744c1ca9620593e95b53ad
Nathaniel Donato <nathanieldonatofx@gmail.com>
2021-05-16 13:01:22 +0200

    format caesar cipher

--- caesars-cipher.js
+++ caesars-cipher.js
@@ -1,13 +1,13 @@
 function rot13(str) {
-  str.toUpperCase(); 
+  str.toUpperCase();
   var set1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"];
   var set2 = ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var arr = str.split(""); 
+  var arr = str.split("");
 
-    for (var i = 0; i < arr.length; i++) { 
-      for (var j = 0; j < set1.length; j++) { 
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < set1.length; j++) {
       if (arr[i] === set1[j]) {
-        arr[i] = set2[j]; 
+        arr[i] = set2[j];
       } else if (arr[i] === set2[j]) {
         arr[i] = set1[j];
       }
